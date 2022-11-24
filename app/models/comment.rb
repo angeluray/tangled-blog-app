c1238c8733e83b37b66b807e72d0ef20c2f61da3
angeluray <aeucampos@gmail.com>
2022-11-24 14:51:01 -0400

    fix comment associations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,11 @@
 class Comment < ApplicationRecord
-  belongs_to :user
-  belongs_to :post
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  belongs_to :post, class_name: 'Post', foreign_key: :post_id
   after_save :update_comments_counter
 
   private
 
   def update_comments_counter
-    post.update(comments_counter: post.comments.length)
+    post.increment!(:comments_counter)
   end
 end
