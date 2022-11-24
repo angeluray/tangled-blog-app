f522859693ce1dd19edd48c33025f71f71c6d3a7
angeluray <aeucampos@gmail.com>
2022-11-24 00:24:06 -0400

    Add comment modal and set relationship

---
+++ app/models/comment.rb
@@ -0,0 +1,11 @@
+class Comment < ApplicationRecord
+  belongs_to :user
+  belongs_to :post
+  after_save :update_comments_counter
+
+  private
+
+  def update_comments_counter
+    post.update(comments_counter: post.comments.length)
+  end
+end
