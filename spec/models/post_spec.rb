3c4144f47d716db667c40cd33cffc5065e89120c
angeluray <aeucampos@gmail.com>
2022-11-25 12:11:30 -0400

    Add last five comments test

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -59,4 +59,9 @@ RSpec.describe Post, type: :model do
     post.save
     expect(user.posts_counter).to eq(6)
   end
+
+  it 'check_last_five_comments should show the last five comments' do
+    expect(subject.check_last_five_comments.length).to be_between(0, 5)
+  end
+
 end
