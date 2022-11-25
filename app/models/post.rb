0522739b188823bb2172dfc11be7b58ab668bfd6
angeluray <aeucampos@gmail.com>
2022-11-25 01:51:37 -0400

    Add validation to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,10 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   after_save :update_posts_counter
 
+  validates :title, presence: true, length: { minimum: 3, maximum: 250 }
+  validates :comments_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def check_last_five_comments
     comments.order(created_at: :desc).limit(5)
   end
