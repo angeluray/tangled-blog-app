61c8356fa4feab59a0678b53a088377bbae2636b
angeluray <aeucampos@gmail.com>
2022-12-01 09:34:15 -0400

    Delete irrelevant migration files

--- db/migrate/20221123002741_remove_user_from_likes.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUserFromLikes < ActiveRecord::Migration[7.0]
-  def change
-    remove_reference :likes, :user, null: false, foreign_key: true
-  end
-end
--- db/migrate/20221123002941_remove_user_from_posts.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUserFromPosts < ActiveRecord::Migration[7.0]
-  def change
-    remove_reference :posts, :user, null: false, foreign_key: true
-  end
-end
