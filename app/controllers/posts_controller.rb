e8b07f66d68e48c962fd5438366cc8bce5e09bd0
angeluray <aeucampos@gmail.com>
2022-12-02 00:01:07 -0400

    Update post controller methods and add 'new' view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,22 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.author = current_user
+
+    @post.save
+    redirect_to user_posts_path(@user)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
