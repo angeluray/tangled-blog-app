9bcf2f1316ec230ea067baf5e04483b7093dd460
angeluray <aeucampos@gmail.com>
2022-12-07 17:07:16 -0400

    Generate migration and update routes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,9 +8,10 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  
   # Do not eager load code on boot.
   config.eager_load = false
-
+  
   # Show full error reports.
   config.consider_all_requests_local = true
 
@@ -29,16 +30,17 @@ Rails.application.configure do
     }
   else
     config.action_controller.perform_caching = false
-
+    
     config.cache_store = :null_store
   end
-
+  
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
-
+  
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
+  
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
